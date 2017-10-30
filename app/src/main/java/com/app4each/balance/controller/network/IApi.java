b92fbeefa9ca5e86b1dba94d8dacc1768ee38af1
VitaliyA <vitabr@gmail.com>
2017-10-30 10:00:45 +0200

    Import helper libs: Retrofit, Realm, EventBus etc.

--- app/src/main/java/com/app4each/balance/controller/network/IApi.java
+++ app/src/main/java/com/app4each/balance/controller/network/IApi.java
@@ -1,8 +1,23 @@
 package com.app4each.balance.controller.network;
 
+import com.app4each.balance.controller.network.results.Result;
+import com.app4each.balance.model.Balance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by vito on 30/10/2017.
  */
 
-public class IApi {
+public interface IApi {
+
+    @GET("bins/iptrr")
+    Call<Balance> getBalance();
+
+    @GET("bins/h1bj3")
+    Call<Result<List<List<Integer>>>> getChart();
+
 }
---
+++ app/src/main/java/com/app4each/balance/controller/network/results/Result.java
@@ -0,0 +1,13 @@
+package com.app4each.balance.controller.network.results;
+
+/**
+ * Created by vito on 30/10/2017.
+ */
+
+/**
+ * Generic Retrofit result object
+ * @param <T>
+ */
+public class Result<T> {
+    public T data;
+}
