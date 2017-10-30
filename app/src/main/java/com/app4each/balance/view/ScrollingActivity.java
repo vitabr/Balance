abbbf0207beeaad242bc9a53ddd3445718dcfc87
VitaliyA <vitabr@gmail.com>
2017-10-30 10:35:24 +0200

    UI build in progress

--- app/src/main/java/com/app4each/balance/view/ScrollingActivity.java
+++ app/src/main/java/com/app4each/balance/view/ScrollingActivity.java
@@ -1,17 +1,51 @@
 package com.app4each.balance.view;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.app4each.balance.R;
+import com.app4each.balance.view.adapters.RecyclerViewAdapter;
+
+import java.util.ArrayList;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerViewAdapter mRecyclerAdapter;
+    private GridLayoutManager mManager;
+    private ArrayList<String> mList = new ArrayList<>();
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    Toast.makeText(ScrollingActivity.this, "message 1", Toast.LENGTH_LONG).show();
+                    return true;
+                case R.id.navigation_dashboard:
+                    Toast.makeText(ScrollingActivity.this, "message 2", Toast.LENGTH_LONG).show();
+                    return true;
+                case R.id.navigation_notifications:
+
+                    Toast.makeText(ScrollingActivity.this, "message 3", Toast.LENGTH_LONG).show();
+                    return true;
+            }
+            return false;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +58,21 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Toast.makeText(ScrollingActivity.this, "" + mList.size(), Toast.LENGTH_LONG).show();
+                mList.add("new Record");
+                mRecyclerAdapter.notifyDataSetChanged();
+
             }
         });
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        refreshRecycler();
+
     }
 
     @Override
@@ -49,5 +94,16 @@ public class ScrollingActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void refreshRecycler() {
+
+        mRecyclerView.setLayoutManager(null);
+        mManager = new GridLayoutManager(ScrollingActivity.this, 1);
+        mRecyclerView.setLayoutManager(mManager);
+        mRecyclerAdapter = new RecyclerViewAdapter(mList);
+        mRecyclerView.setAdapter(mRecyclerAdapter);
+        mRecyclerAdapter.notifyDataSetChanged();
+
     }
 }
---
+++ app/src/main/java/com/app4each/balance/view/adapters/RecyclerViewAdapter.java
@@ -0,0 +1,82 @@
+package com.app4each.balance.view.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.app4each.balance.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vito on 30/10/2017.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static ArrayList<String> mTempList;
+
+
+    public RecyclerViewAdapter(ArrayList<String> movies) {
+        this.mTempList = movies;
+    }
+
+    //Holder
+    //RecyclerView
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+        Context mContext;
+        //Create viewHolder Constructor
+
+        public ViewHolder(final View v) {
+            super(v);
+
+            mContext = v.getContext();
+
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Toast.makeText(mContext,"Clicked",Toast.LENGTH_LONG).show();
+                }
+            });
+
+            title = v.findViewById(R.id.title_card);
+
+
+        }
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.card_item,parent , false); //row_card for linear
+
+        ViewHolder mHolder = new ViewHolder(v);
+
+
+        return mHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder mHolder, int position) {
+
+        mHolder.title.setText(mTempList.get(position));
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTempList.size();
+    }
+}
